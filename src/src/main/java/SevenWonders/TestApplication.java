546a9241f8e9ba077c1478ca841c7014abf7ec0a
Dogac <dogacel@gmail.com>
2019-10-28 14:29:00 +0300

    Updated gradle to work with javafx

---
+++ src/src/main/java/SevenWonders/TestApplication.java
@@ -0,0 +1,16 @@
+package SevenWonders;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TestApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
