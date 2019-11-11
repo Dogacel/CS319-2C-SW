f44356e6d6f001a85cb5f93de26fdd6ba09bf571
busra.buyukgebiz <busrabgz@gmail.com>
2019-11-12 02:12:25 +0300

    SceneManager

---
+++ src/src/main/java/SevenWonders/controller/SceneManager.java
@@ -0,0 +1,44 @@
+package SevenWonders.controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneManager {
+    private static SceneManager instance = new SceneManager();
+    private FXMLLoader fxmlLoader;
+    private Stage stage;
+
+
+    private SceneManager() {
+        fxmlLoader = new FXMLLoader();
+    }
+
+    public static SceneManager getInstance() {
+        return instance;
+    }
+
+    private void changeScene(String sceneName) throws IOException {
+
+        Parent root = getSceneByName(sceneName);
+        Scene scene = stage.getScene();
+        if (scene == null) {
+            scene = new Scene(root);
+            stage.setScene(scene);
+        } else {
+            scene.setRoot(root);
+        }
+    }
+
+    public void actOnExit() {
+        stage.close();
+
+    }
+
+    private Parent getSceneByName(String sceneName) throws IOException {
+        return fxmlLoader.load(getClass().getResource(sceneName));
+    }
+}
