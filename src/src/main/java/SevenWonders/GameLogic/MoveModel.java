53dc0f61eb29ffbcd903d4830bd4f73d1322a999
Ozan <ozanaydinn07@gmail.com>
2019-11-12 22:38:08 +0300

    TODO trades

--- src/src/main/java/SevenWonders/GameLogic/MoveModel.java
+++ src/src/main/java/SevenWonders/GameLogic/MoveModel.java
@@ -6,7 +6,7 @@ public class MoveModel {
     private int playerID;
     private int selectedCardID;
     private ACTION_TYPE action;
-    //private TradeAction[] trades;
+    //private TradeAction[] trades;  TODO
 
     public MoveModel(int playerID, int selectedCardID, ACTION_TYPE action)
     {
