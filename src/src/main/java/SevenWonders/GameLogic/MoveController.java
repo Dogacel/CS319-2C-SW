5691e0a22c34e0fbef1fc1c14a68ca713220586f
Ozan <ozanaydinn07@gmail.com>
2019-11-14 14:28:27 +0300

    Update on CanPlayerMakeTheTrade

--- src/src/main/java/SevenWonders/GameLogic/MoveController.java
+++ src/src/main/java/SevenWonders/GameLogic/MoveController.java
@@ -1,5 +1,6 @@
 package SevenWonders.GameLogic;
 
+import SevenWonders.GameLogic.Enums.ACTION_TYPE;
 import SevenWonders.GameLogic.Enums.CARD_EFFECT_TYPE;
 import SevenWonders.GameLogic.Enums.RESOURCE_TYPE;
 import javafx.util.Pair;
@@ -20,16 +21,61 @@ public class MoveController {
     }
 
 
-    public boolean playerCanMakeMove(MoveModel moveModel, PlayerModel currentPlayer, Pair<PlayerModel, PlayerModel> neighbours) {
+    public boolean playerCanMakeMove(MoveModel moveModel, PlayerModel currentPlayer) {
         Card selectedCard = fromIDToCard(moveModel.getSelectedCardID());
     }
 
-    private boolean playerCanMakeTheTrade( MoveModel move, PlayerModel currentPlayer, Pair<PlayerModel, PlayerModel> neighbours) {
+    private boolean playerCanMakeTheTrade( MoveModel move, PlayerModel currentPlayer) {
+        int goldOfCurrentPlayer = currentPlayer.getGold();
+
+        /* boolean for understanding if there is a discount */
+        boolean hasRightRawDiscount = false;
+        boolean hasLeftRawDiscount = false;
+        boolean hasManifacturedDiscount = false;
+
+        RESOURCE_TYPE[] rawResources = { RESOURCE_TYPE.WOOD, RESOURCE_TYPE.STONE, RESOURCE_TYPE.ORE, RESOURCE_TYPE.BRICK};
+        RESOURCE_TYPE[] manifacturedResources = { RESOURCE_TYPE.GLASS, RESOURCE_TYPE.PAPYRUS, RESOURCE_TYPE.LOOM};
+
+        for (Card card : currentPlayer.getConstructionZone().getConstructedCards()) {
+            CARD_EFFECT_TYPE effect = card.getCardEffect().getEffectType();
+
+            switch (effect) {
+                case RIGHT_RAW_MATERIAL_TRADE_DISCOUNT:
+                    hasRightRawDiscount = true;
+                    break;
+                case LEFT_RAW_MATERIAL_TRADE_DISCOUNT:
+                    hasLeftRawDiscount = true;
+                    break;
+                case MANUFACTURED_GOODS_TRADE_DISCOUNT:
+                    hasManifacturedDiscount = true;
+                    break;
+                default: break;
+            }
+        }
+
+        for (TradeAction trade : move.getTrades()) {
+            if ( trade.getSelectedResource())
+        }
 
     }
 
     private boolean playerHasEnoughCoins(MoveModel moveModel, PlayerModel currentPlayer) {
+        ACTION_TYPE action = moveModel.getAction();
+        TradeAction[] trades = moveModel.getTrades();
+        int goldOfCurrentPlayer = currentPlayer.getGold();
+        int goldCostForCurrentCard = fromIDToCard(moveModel.getSelectedCardID()).getRequirements().get(RESOURCE_TYPE.GOLD);
 
+        for ()
+
+        switch ( action) {
+            case DISCARD_CARD:
+            case UPGRADE_WONDER:
+            case USE_GOD_POWER:
+                return true;
+            case BUILD_CARD:
+                return goldOfCurrentPlayer >= goldCostForCurrentCard;
+            default: return false;
+        }
     }
 
     //  TODO add trade action here!
@@ -86,7 +132,7 @@ public class MoveController {
 
     }
 
-    /* TO DO
+    /* TODO
     This method is for taking an ID and returning a Card object, maybe initialized somewhere else later.
      */
     public Card fromIDToCard(int cardID) {
