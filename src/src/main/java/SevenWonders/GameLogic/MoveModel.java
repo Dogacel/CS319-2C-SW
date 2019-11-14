6a6e83cdc7a20443c80f9b34463d62966ec8e4b8
Ozan <ozanaydinn07@gmail.com>
2019-11-14 14:27:55 +0300

    Update TradeAction array to ArrayList

--- src/src/main/java/SevenWonders/GameLogic/MoveModel.java
+++ src/src/main/java/SevenWonders/GameLogic/MoveModel.java
@@ -2,17 +2,20 @@ package SevenWonders.GameLogic;
 
 import SevenWonders.GameLogic.Enums.ACTION_TYPE;
 
+import java.util.ArrayList;
+
 public class MoveModel {
     private int playerID;
     private int selectedCardID;
     private ACTION_TYPE action;
-    //private TradeAction[] trades;  TODO
+    private ArrayList<TradeAction> trades; //ask
 
     public MoveModel(int playerID, int selectedCardID, ACTION_TYPE action)
     {
         this.playerID = playerID;
         this.selectedCardID = selectedCardID;
         this.action = action;
+        this.trades = new ArrayList<TradeAction>();
     }
 
     public int getPlayerID(){
@@ -25,5 +28,19 @@ public class MoveModel {
 
     public ACTION_TYPE getAction(){
         return action;
+    }
+
+    public ArrayList<TradeAction> getTrades() {
+        return trades;
+    }
+
+    public boolean addTrades( TradeAction trade) {
+        if ( trades.contains(trade)) {
+            throw new IllegalArgumentException("The value is already in the list."); //discuss
+        }
+        else {
+            trades.add(trade);
+            return true;
+        }
     }
 }
