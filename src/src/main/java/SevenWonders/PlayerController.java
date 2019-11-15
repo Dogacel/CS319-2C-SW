613b362251efc7baa70e63866406b1cef6d1aa76
busra.buyukgebiz <busrabgz@gmail.com>
2019-11-15 22:23:43 +0300

    add player controller and model class

---
+++ src/src/main/java/SevenWonders/PlayerController.java
@@ -0,0 +1,11 @@
+package SevenWonders;
+
+import javafx.scene.layout.BackgroundImage;
+
+public class PlayerController {
+    public PlayerModel model;
+
+    public PlayerController( PlayerModel model){
+        this.model = model;
+    }
+}
