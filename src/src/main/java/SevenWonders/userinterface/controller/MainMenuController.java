4dc6e5f118159f4db9927fe6fd2cfe5bc08f98f2
Burak <burakokumus98@gmail.com>
2019-11-17 19:09:21 +0300

    Add fxml for MainMenu

---
+++ src/src/main/java/SevenWonders/userinterface/controller/MainMenuController.java
@@ -0,0 +1,12 @@
+package SevenWonders.userinterface.controller;
+
+import SevenWonders.userinterface.model.MainMenuModel;
+import com.sun.tools.javac.Main;
+
+public class MainMenuController {
+    public MainMenuModel model;
+
+    public MainMenuController(MainMenuModel model) {
+        this.model = model;
+    }
+}
