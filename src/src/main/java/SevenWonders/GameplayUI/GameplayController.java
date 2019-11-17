8fa7d645172b8e8dacc38aee324c955b02cf3710
Büşra Büyükgebiz <busrabgz@gmail.com>
2019-11-18 01:09:21 +0300

    Update Gameplay

--- src/src/main/java/SevenWonders/GameplayUI/GameplayController.java
+++ src/src/main/java/SevenWonders/GameplayUI/GameplayController.java
@@ -17,6 +17,18 @@ public class GameplayController implements Initializable{
     @FXML
     Pane playerViewPane;
 
+    @FXML
+    Pane neighborViewRightPane;
+
+    @FXML
+    Pane neighborViewLeftPane;
+
+    @FXML
+    Pane otherPlayersViewPane;
+
+    @FXML
+    Pane cardViewPane;
+
     public GameplayController() {
         this.model = new GameplayModel();
     }
@@ -24,6 +36,14 @@ public class GameplayController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Pane newLoadedPane =  (Pane) AssetManager.getInstance().getSceneByNameForce("PlayerView.fxml");
+        Pane newLoadedPane2 =  (Pane) AssetManager.getInstance().getSceneByNameForce("RightNeighborView.fxml");
+        Pane newLoadedPane3 = (Pane) AssetManager.getInstance().getSceneByNameForce("OtherPlayersView.fxml");
+       // Pane newLoadedPane4 = (Pane) AssetManager.getInstance().getSceneByNameForce("CardView.fxml");
+        Pane newLoadedPane5 = (Pane) AssetManager.getInstance().getSceneByNameForce("LeftNeighborView.fxml");
+        this.neighborViewRightPane.getChildren().add(newLoadedPane2);
+        this.neighborViewLeftPane.getChildren().add(newLoadedPane5);
         this.playerViewPane.getChildren().add(newLoadedPane);
+        this.otherPlayersViewPane.getChildren().add( newLoadedPane3);
+      //  this.cardViewPane.getChildren().add( newLoadedPane4);
     }
 }
