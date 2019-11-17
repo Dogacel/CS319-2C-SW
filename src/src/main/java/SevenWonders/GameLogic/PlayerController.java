d8f4b21986ade01dec6d67d5a1898dd07dc9d2c9
Mert Aslan <aslan-mert99@hotmail.com>
2019-11-17 16:44:31 +0300

    Add makeMove to PlayerController, update PlayerModel

---
+++ src/src/main/java/SevenWonders/GameLogic/PlayerController.java
@@ -0,0 +1,88 @@
+package SevenWonders.GameLogic;
+
+import SevenWonders.AssetManager;
+import SevenWonders.GameLogic.Enums.ACTION_TYPE;
+
+public class PlayerController {
+    private final int NO_OF_PLAYERS = 7;
+    private final int DISCARD_REWARD = 3;
+
+    private PlayerModel player;
+    private GameController gameController;
+
+    public PlayerController(PlayerModel player, GameController gameController){
+
+        this.player = player;
+        this.gameController = gameController;
+    }
+
+    public void updateCurrentMove(MoveModel move){
+        player.setCurrentMove(move);
+    }
+
+    public void makeMove(){
+        MoveModel move = player.getCurrentMove();
+        ACTION_TYPE action = move.getAction();
+
+        switch (action){
+            case DISCARD_CARD:
+                gameController.discardCard(AssetManager.getInstance().getCardByID(move.getSelectedCardID()));
+
+                player.setGold(player.getGold() + DISCARD_REWARD);
+
+                for (Card card : player.getHand())
+                {
+                    if (card.getId() == move.getSelectedCardID()) {
+                        //Played card is found, remove it from players hand
+                        player.getHand().remove(card);
+                        break; //Remove only one card
+                    }
+                }
+                break;
+            case BUILD_CARD:
+                player.getConstructionZone().buildCard(AssetManager.getInstance().getCardByID(move.getSelectedCardID()));
+
+                for (Card card : player.getHand())
+                {
+                    if (card.getId() == move.getSelectedCardID()) {
+                        //Played card is found, remove it from players hand
+                        player.getHand().remove(card);
+                        break; //Remove only one card
+                    }
+                }
+                break;
+            case UPGRADE_WONDER:
+
+                player.getWonder().upgradeStage();
+
+                for (Card card : player.getHand())
+                {
+                    if (card.getId() == move.getSelectedCardID()) {
+                        //Played card is found, remove it from players hand
+                        player.getHand().remove(card);
+                        break; //Remove only one card
+                    }
+                }
+                break;
+            case USE_GOD_POWER:
+                //TODO Add God Power
+                break;
+
+        }
+
+
+    }
+
+    public MoveModel getCurrentMove(){ return player.getCurrentMove(); }
+
+    public void setShields(int shields){ player.setShields(shields); }
+
+    public void setGold(int gold){ player.setGold(gold); }
+
+    public int getGold(){ return player.getGold(); }
+
+    public int getShields() { return player.getShields(); }
+
+
+
+}
