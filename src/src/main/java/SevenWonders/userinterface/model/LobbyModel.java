183c6ba20f5962b6c14913cf219b2fcda6443371
Burak <burakokumus98@gmail.com>
2019-11-18 16:29:35 +0300

    Add models for lobby and settings

---
+++ src/src/main/java/SevenWonders/userinterface/model/LobbyModel.java
@@ -0,0 +1,12 @@
+package SevenWonders.userinterface.model;
+
+public class LobbyModel {
+    private String barImage;
+    private String gameId;
+    //private User[] users;
+
+    public LobbyModel()
+    {
+
+    }
+}
