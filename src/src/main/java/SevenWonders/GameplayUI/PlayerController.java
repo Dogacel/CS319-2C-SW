5b7e6e332584667d0fa1894a7f9c871783d743f7
busra.buyukgebiz <busrabgz@gmail.com>
2019-11-18 19:51:28 +0300

    player view button listeners added

--- src/src/main/java/SevenWonders/GameplayUI/PlayerController.java
+++ src/src/main/java/SevenWonders/GameplayUI/PlayerController.java
@@ -1,9 +1,8 @@
 package SevenWonders.GameplayUI;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -26,38 +25,101 @@ public class PlayerController implements Initializable {
     @FXML
     Button godPowerButton;
 
+    @FXML
+    Button settingsButton;
+
+    @FXML
+    Button tutorialButton;
+
+    @FXML
+    Button exitButton;
+
 
     public PlayerController(){
         this.model = new PlayerModel();
     }
 
     @FXML
-    private void readyButtonClicked(ActionEvent event) {
+    private void buildCardButtonPressed(MouseEvent event) {
+        buildCardButton.setStyle("-fx-background-image: url('/ui-images/buildCardButtonClicked.png')");
+    }
+
+    @FXML
+    private void buildCardButtonReleased(MouseEvent event) {
+        buildCardButton.setStyle("-fx-background-image: url('/ui-images/buildCardButton.png')");
+    }
+
+    @FXML
+    private void buildWonderButtonPressed(MouseEvent event) {
+        buildWonderButton.setStyle("-fx-background-image: url('/ui-images/buildWonderButtonClicked.png')");
+    }
+
+    @FXML
+    private void buildWonderButtonReleased(MouseEvent event) {
+        buildWonderButton.setStyle("-fx-background-image: url('/ui-images/buildWonderButton.png')");
+    }
+
+    @FXML
+    private void discardButtonPressed(MouseEvent event) {
+        discardButton.setStyle("-fx-background-image: url('/ui-images/discardCardButtonClicked.png')");
+    }
+
+    @FXML
+    private void discardButtonReleased(MouseEvent event) {
+        discardButton.setStyle("-fx-background-image: url('/ui-images/discardCardButton.png')");
+    }
+
+    @FXML
+    private void godPowerButtonPressed(MouseEvent event) {
+        godPowerButton.setStyle("-fx-background-image: url('/ui-images/godPowerButtonClicked.png')");
+    }
+
+    @FXML
+    private void godPowerButtonReleased(MouseEvent event) {
+        godPowerButton.setStyle("-fx-background-image: url('/ui-images/godPowerButton.png')");
+    }
+
+    @FXML
+    private void settingsButtonPressed(MouseEvent event) {
+        settingsButton.setStyle("-fx-background-image: url('/ui-images/settingsButtonClicked.png')");
+    }
+
+    @FXML
+    private void settingsButtonReleased(MouseEvent event) {
+        settingsButton.setStyle("-fx-background-image: url('/ui-images/settingsButton.png')");
+    }
+
+    @FXML
+    private void tutorialButtonPressed(MouseEvent event) {
+        tutorialButton.setStyle("-fx-background-image: url('/ui-images/tutorialButtonClicked.png')");
+    }
+
+    @FXML
+    private void tutorialButtonReleased(MouseEvent event) {
+        tutorialButton.setStyle("-fx-background-image: url('/ui-images/tutorialButton.png')");
+    }
+
+    @FXML
+    private void exitButtonPressed(MouseEvent event) {
+        exitButton.setStyle("-fx-background-image: url('/ui-images/exitButtonClicked.png')");
+    }
+
+    @FXML
+    private void exitButtonReleased(MouseEvent event) {
+        exitButton.setStyle("-fx-background-image: url('/ui-images/exitButton.png')");
+    }
+
+    @FXML
+    private void readyButtonPressed(MouseEvent event) {
         readyButton.setStyle("-fx-background-image: url('/ui-images/tokens/readyClicked.png')");
     }
 
     @FXML
-    private void buildCardButtonClicked(ActionEvent event) {
-        //buildCardButton.setStyle("");
-    }
-
-    @FXML
-    private void buildWonderButtonClicked(ActionEvent event) {
-        //buildWonderButton.setStyle("");
-    }
-
-    @FXML
-    private void discardButtonClicked(ActionEvent event) {
-        //discardButton.setStyle("");
-    }
-
-    @FXML
-    private void godPowerButtonClicked(ActionEvent event) {
-        //godPowerButton.setStyle("");
+    private void readyButtonReleased(MouseEvent event) {
+        readyButton.setStyle("-fx-background-image: url('/ui-images/tokens/ready.png')");
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        readyButton.setOnAction(this::readyButtonClicked);
     }
 }
