69d4440f9bce1254161918290666199757b9c017
Ozan <ozanaydinn07@gmail.com>
2019-11-18 19:48:31 +0300

    MainMenuController created

--- src/src/main/java/SevenWonders/GameplayUI/MainMenuController.java
+++ src/src/main/java/SevenWonders/GameplayUI/MainMenuController.java
@@ -1,9 +1,63 @@
 package SevenWonders.GameplayUI;
+import SevenWonders.SceneManager;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.application.Platform;
 
-public class MainMenuController {
-    public MainMenuModel model;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 
-    public MainMenuController(MainMenuModel model) {
-        this.model = model;
+public class MainMenuController implements Initializable{
+
+    @FXML
+    Button playButton;
+    @FXML
+    Button settingsButton;
+    @FXML
+    Button tutorialButton;
+    @FXML
+    Button exitButton;
+
+    @FXML
+    public void playButtonPressed( ActionEvent event) {
     }
+    @FXML
+    public void playButtonReleased( ActionEvent event) {
+        SceneManager.getInstance().changeScene("PlayMenu.fxml");
+    }
+    @FXML
+    public void settingsButtonPressed( ActionEvent event) {
+
+    }
+    @FXML
+    public void settingsButtonReleased( ActionEvent event) {
+        SceneManager.getInstance().changeScene("Settings.fxml");
+    }
+    @FXML
+    public void tutorialButtonPressed( ActionEvent event) {
+
+    }
+    @FXML
+    public void tutorialButtonReleased( ActionEvent event) {
+        SceneManager.getInstance().changeScene("Settings.fxml"); //TODO add tutorial
+    }
+    @FXML
+    public void exitButtonPressed( ActionEvent event) {
+
+    }
+    @FXML
+    public void exitButtonReleased( ActionEvent event) {
+        Platform.exit();
+        System.exit(0);
+    }
+
+
+    @Override
+    public void initialize( URL url, ResourceBundle bundle) {
+
+    }
+
 }
