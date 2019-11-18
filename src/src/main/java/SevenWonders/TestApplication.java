4123758b1663fc836ae39501ae44e389bcd9148e
Dogac <dogacel@gmail.com>
2019-11-18 23:55:01 +0300

    Change to main menu

--- src/src/main/java/SevenWonders/TestApplication.java
+++ src/src/main/java/SevenWonders/TestApplication.java
@@ -7,7 +7,7 @@ public class TestApplication extends Application {
 
     @Override
     public void start(Stage primaryStage){
-        SceneManager.getInstance().changeScene("PlayMenu.fxml");
+        SceneManager.getInstance().changeScene("MainMenu.fxml");
         SceneManager.getInstance().show();
     }
 
