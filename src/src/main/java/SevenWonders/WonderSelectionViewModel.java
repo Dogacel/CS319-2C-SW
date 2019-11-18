c28b7850bc9ee2f8ee34190c9994de5df69bf18d
Yuce <yuceyuce99@gmail.com>
2019-11-18 20:48:13 +0300

    Visually upgrade Wonder Selection View

--- src/src/main/java/SevenWonders/WonderSelectionViewModel.java
+++ src/src/main/java/SevenWonders/WonderSelectionViewModel.java
@@ -1,4 +1,18 @@
 package SevenWonders;
 
+import SevenWonders.GameLogic.Enums.WONDER_TYPE;
+
 public class WonderSelectionViewModel {
+    private WONDER_TYPE selectedWonder;
+    public WonderSelectionViewModel(){
+        selectedWonder = null;
+    }
+
+    public void setSelectedWonder(WONDER_TYPE selectedWonder) {
+        this.selectedWonder = selectedWonder;
+    }
+
+    public WONDER_TYPE getSelectedWonder(){
+        return selectedWonder;
+    }
 }
