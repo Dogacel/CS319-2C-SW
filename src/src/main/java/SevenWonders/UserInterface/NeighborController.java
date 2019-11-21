5ef4c873b7b9e98561b05e3fb9f626124f8242f6
busra.buyukgebiz <busrabgz@gmail.com>
2019-11-21 20:32:01 +0300

    update neighbor view

--- src/src/main/java/SevenWonders/UserInterface/NeighborController.java
+++ src/src/main/java/SevenWonders/UserInterface/NeighborController.java
@@ -1,7 +1,31 @@
 package SevenWonders.UserInterface;
 
+import SevenWonders.GameLogic.PlayerModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
 public class NeighborController {
 
+    PlayerModel neıghbor;
+
+    @FXML
+    ImageView neighborIcon;
+
+    @FXML
+    Label neighborName;
+
+    @FXML
+    GridPane resourcesGrid;
+
     public NeighborController() {
+
+        updateScene();
     }
+
+    public void updateScene(){
+
+    }
+
 }
