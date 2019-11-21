738ff386e95f7bcda194894a3fe49a0c15dc08d1
Ozan <ozanaydinn07@gmail.com>
2019-11-21 21:41:22 +0300

    Update OtherPlayer controllers

--- src/src/main/java/SevenWonders/UserInterface/OtherPlayersController.java
+++ src/src/main/java/SevenWonders/UserInterface/OtherPlayersController.java
@@ -1,60 +1,22 @@
 package SevenWonders.UserInterface;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-public class OtherPlayersController {
-    GameplayController gameplayController;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OtherPlayersController implements Initializable {
 
     @FXML
-    ImageView ageIcon;
+    
 
-    @FXML
-    Button player3Button, player4Button, player5Button, player6Button;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
-    public OtherPlayersController(){
-    }
-/*
-    private void setClickedStyle(Button button, String imageName){
-        button.setStyle("-fx-background-image: url('/ui-images/otherPlayer_' + )");
-    }*/
-
-    @FXML
-    private void player3ButtonClicked(MouseEvent event) {
-       // player3Button.setStyle("-fx-background-image: url('/ui-images/other')");
-        gameplayController.player3ButtonClicked();
-    }
-
-    @FXML
-    private void player4ButtonClicked(MouseEvent event) {
-        //player4Button.setStyle("-fx-background-image: url('/ui-images/')");
-        gameplayController.player4ButtonClicked();
-    }
-
-    @FXML
-    private void player5ButtonClicked(MouseEvent event) {
-        //player5Button.setStyle("-fx-background-image: url('/ui-images/')");
-        gameplayController.player5ButtonClicked();
-    }
-
-    @FXML
-    private void player6ButtonClicked(MouseEvent event) {
-        //player6Button.setStyle("-fx-background-image: url('/ui-images/')");
-        gameplayController.player6ButtonClicked();
-    }
-
-    public void changeAgeIcon( int age){
-        if( age == 1){
-            ageIcon.setImage( new Image("/ui-images/tokens/age1.png"));
-        }
-        else if( age == 2){
-            ageIcon.setImage( new Image("/ui-images/tokens/age2.png"));
-        }
-        else{
-            ageIcon.setImage( new Image("/ui-images/tokens/age3.png"));
-        }
     }
 }
