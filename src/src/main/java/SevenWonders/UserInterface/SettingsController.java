1fb115402a208c754714abf503d631599427ad0b
busra.buyukgebiz <busrabgz@gmail.com>
2019-12-01 02:25:38 +0300

    Design settings view

--- src/src/main/java/SevenWonders/UserInterface/SettingsController.java
+++ src/src/main/java/SevenWonders/UserInterface/SettingsController.java
@@ -1,8 +1,25 @@
 package SevenWonders.UserInterface;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class SettingsController {
 
-    public SettingsController()
-    {
+    @FXML
+    Button closeButton;
+
+    @FXML
+    AnchorPane settingsAnchor;
+
+    @FXML
+    public void closeButtonClicked( MouseEvent event) {
+        settingsAnchor.setVisible(false);
+    }
+
+    @FXML
+    public void updateScene() {
+        settingsAnchor.setVisible(true);
     }
 }
