16b61a394b927f2d1677c04c972575fdb9ec8141
busra.buyukgebiz <busrabgz@gmail.com>
2019-12-14 00:16:48 +0300

    Create score view and controller

---
+++ src/src/main/java/SevenWonders/UserInterface/ScoreViewController.java
@@ -0,0 +1,32 @@
+package SevenWonders.UserInterface;
+
+import SevenWonders.Network.Requests.LobbyUpdateRequest;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+import java.awt.event.MouseEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ScoreViewController implements Initializable {
+    @FXML
+    private VBox scoreVBox;
+
+    @FXML
+    private Button exitButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void exitButtonPressed(MouseEvent e) {
+
+    }
+
+    public void returnMenuButtonPressed(MouseEvent e) {
+
+    }
+}
