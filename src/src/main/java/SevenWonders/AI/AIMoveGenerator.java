87d4bd891e537ccafa57d0c0841d728ca680aea6
Dogac <dogacel@gmail.com>
2019-12-16 20:09:35 +0300

    Fix AI move to discard a card for now.

--- src/src/main/java/SevenWonders/AI/AIMoveGenerator.java
+++ src/src/main/java/SevenWonders/AI/AIMoveGenerator.java
@@ -1,12 +1,16 @@
 package SevenWonders.AI;
 
+import SevenWonders.GameLogic.Deck.Card.Card;
+import SevenWonders.GameLogic.Enums.ACTION_TYPE;
 import SevenWonders.GameLogic.Enums.AI_DIFFICULTY;
 import SevenWonders.GameLogic.Game.GameModel;
+import SevenWonders.GameLogic.Move.MoveController;
 import SevenWonders.GameLogic.Move.MoveModel;
+import SevenWonders.GameLogic.Move.TradeAction;
+import SevenWonders.GameLogic.Player.PlayerModel;
 
 public class AIMoveGenerator {
-    public static MoveModel generateMove(GameModel game, AI_DIFFICULTY difficulty) {
-        // TODO: Generate move
-        return null;
+    public static MoveModel generateMove(PlayerModel player, AI_DIFFICULTY difficulty) {
+        return new MoveModel(player.getId(), player.getHand().firstElement().getId(), ACTION_TYPE.DISCARD_CARD);
     }
 }
