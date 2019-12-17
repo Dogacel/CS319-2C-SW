764e993e318ce6bc84aaea498ef68f674c0c2d79
burakokumus <burakokumus98@gmail.com>
2019-12-17 22:31:56 +0300

    Set text and image in the tutorial view according to the page number

--- src/src/main/java/SevenWonders/UserInterface/TutorialController.java
+++ src/src/main/java/SevenWonders/UserInterface/TutorialController.java
@@ -2,15 +2,21 @@ package SevenWonders.UserInterface;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+
 
 public class TutorialController {
 
     public int currentPageNo;
 
-    public final int totalPageNo = 3;
+    public final int totalPageNo = 2;
 
+    Image img;
+    //img = new Image("images/cards/age1.png");
     @FXML
     Button closeButton;
 
@@ -24,6 +30,12 @@ public class TutorialController {
     GridPane tutorialGrid;
 
     @FXML
+    Label tutorialTextLabel;
+
+    @FXML
+    ImageView tutorialImage;
+
+    @FXML
     public void closeButtonClicked( MouseEvent event) {
         tutorialGrid.setVisible(false);
     }
@@ -35,6 +47,9 @@ public class TutorialController {
             leftButton.setVisible(false);
         }
         rightButton.setVisible(true);
+        tutorialTextLabel.setText("Tutorial page " + (currentPageNo + 1));
+        img = new Image("images/cards/age" + (currentPageNo + 1) + ".png");
+        tutorialImage.setImage(img);
     }
 
     @FXML
@@ -44,11 +59,18 @@ public class TutorialController {
             rightButton.setVisible(false);
         }
         leftButton.setVisible(true);
+        tutorialTextLabel.setText("Tutorial page " + (currentPageNo + 1));
+        img = new Image("images/cards/age" + (currentPageNo + 1) + ".png");
+        tutorialImage.setImage(img);
+
     }
 
     public void updateScene() {
         tutorialGrid.setVisible(true);
         currentPageNo = 0;
         leftButton.setVisible(false);
+        tutorialTextLabel.setText("Tutorial page " + (currentPageNo + 1));
+        img = new Image("images/cards/age1.png");
+        tutorialImage.setImage(img);
     }
 }
