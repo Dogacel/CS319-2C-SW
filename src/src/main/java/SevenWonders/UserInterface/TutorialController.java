f13771a37828ae616da9b9003690a758298443bd
burakokumus <burakokumus98@gmail.com>
2019-12-17 21:54:57 +0300

    Set visibility of buttons in tutorial view

--- src/src/main/java/SevenWonders/UserInterface/TutorialController.java
+++ src/src/main/java/SevenWonders/UserInterface/TutorialController.java
@@ -3,21 +3,52 @@ package SevenWonders.UserInterface;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class TutorialController {
+
+    public int currentPageNo;
+
+    public final int totalPageNo = 3;
+
     @FXML
     Button closeButton;
 
     @FXML
-    AnchorPane tutorialAnchor;
+    Button leftButton;
+
+    @FXML
+    Button rightButton;
+
+    @FXML
+    GridPane tutorialGrid;
 
     @FXML
     public void closeButtonClicked( MouseEvent event) {
-        tutorialAnchor.setVisible(false);
+        tutorialGrid.setVisible(false);
+    }
+
+    @FXML
+    public void leftButtonClicked( MouseEvent event) {
+        currentPageNo--;
+        if(currentPageNo == 0){
+            leftButton.setVisible(false);
+        }
+        rightButton.setVisible(true);
+    }
+
+    @FXML
+    public void rightButtonClicked( MouseEvent event) {
+        currentPageNo++;
+        if(currentPageNo == totalPageNo){
+            rightButton.setVisible(false);
+        }
+        leftButton.setVisible(true);
     }
 
     public void updateScene() {
-        tutorialAnchor.setVisible(true);
+        tutorialGrid.setVisible(true);
+        currentPageNo = 0;
+        leftButton.setVisible(false);
     }
 }
