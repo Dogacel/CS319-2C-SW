d466bc4d256e21a6acbf0c786271728aa20352b0
Dogac <dogacel@gmail.com>
2019-12-18 15:28:38 +0300

    Fix recursive method and green card construction zone view

--- src/src/main/java/SevenWonders/UserInterface/ConstructionZoneController.java
+++ src/src/main/java/SevenWonders/UserInterface/ConstructionZoneController.java
@@ -36,6 +36,7 @@ public class ConstructionZoneController {
         brown.getChildren().clear();
         gray.getChildren().clear();
         blue.getChildren().clear();
+        green.getChildren().clear();
         red.getChildren().clear();
         yellowAndPurple.getChildren().clear();
         for(Card card: playerModel.getConstructionZone().getConstructedCards()){
