13072b8d385c2ebdd3bf50c85904f6858e140ba1
burakokumus <burakokumus98@gmail.com>
2019-12-20 17:04:23 +0300

    Add controller for hero power selection

---
+++ src/src/main/java/SevenWonders/UserInterface/HeroPowerSelectionController.java
@@ -0,0 +1,81 @@
+package SevenWonders.UserInterface;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class HeroPowerSelectionController {
+    @FXML
+    Button button1;
+    @FXML
+    Button button2;
+    @FXML
+    Button button3;
+    @FXML
+    Button button4;
+    @FXML
+    Button button5;
+    @FXML
+    Button button6;
+    @FXML
+    Button button7;
+    @FXML
+    Button button8;
+    @FXML
+    Button button9;
+    @FXML
+    Button button10;
+    @FXML
+    Button button11;
+    @FXML
+    Button button12;
+
+    @FXML
+    public void button1Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void button2Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void button3Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void button4Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void button5Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void button6Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void button7Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void button8Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void button9Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void button10Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void button11Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void button12Clicked(MouseEvent mouseEvent) {
+    }
+
+}
