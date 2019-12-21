6425c146252da1eacc388261fb0824b41f586876
busra.buyukgebiz <busrabgz@gmail.com>
2019-12-21 17:20:09 +0300

    Add exit button listener, card glow on clicked, border if can build

--- src/src/main/java/SevenWonders/UserInterface/CardViewController.java
+++ src/src/main/java/SevenWonders/UserInterface/CardViewController.java
@@ -2,17 +2,22 @@ package SevenWonders.UserInterface;
 
 import SevenWonders.AssetManager;
 import SevenWonders.GameLogic.Deck.Card.Card;
+import SevenWonders.GameLogic.Enums.ACTION_TYPE;
+import SevenWonders.GameLogic.Move.MoveController;
+import SevenWonders.GameLogic.Move.MoveModel;
+import SevenWonders.Network.Server;
+import SevenWonders.SceneManager;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 
-import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Vector;
@@ -28,7 +33,7 @@ public class CardViewController implements Initializable {
     HBox cardBox;
 
     @FXML
-    Button discardPile;
+    Button discardPile, exitButton;
 
     @FXML
     BorderPane leftPane;
@@ -55,7 +60,23 @@ public class CardViewController implements Initializable {
 
     private void updateCards(Vector<Card> hand){
         cardBox.getChildren().clear();
+        int depth1 = 100;
+        int depth2 = 5;
+        DropShadow borderGlow= new DropShadow();
+        borderGlow.setOffsetY(0f);
+        borderGlow.setOffsetX(0f);
+        borderGlow.setColor(Color.GOLD);
+        borderGlow.setWidth(depth1);
+        borderGlow.setHeight(depth1);
+
         for (int i = 0 ; i < hand.size() ; i++) {
+            DropShadow canBuild = new DropShadow();
+            canBuild.setOffsetY(0f);
+            canBuild.setOffsetX(0f);
+            canBuild.setWidth(depth2);
+            canBuild.setHeight(depth2);
+            canBuild.setSpread(1);
+
             if (hand.get(i) != null) {
                 ImageView imageView = new ImageView(
                         AssetManager.getInstance().getImage(hand.get(i).getName().replaceAll(" ", "").toLowerCase() + ".png")
@@ -63,16 +84,52 @@ public class CardViewController implements Initializable {
                 imageView.setScaleX(0.95);
                 imageView.setScaleY(0.95);
                 Card c = hand.get(i);
+                MoveModel move = new MoveModel(gameplayController.getPlayer().getId(), c.getId(), ACTION_TYPE.BUILD_CARD);
+
+                if(MoveController.getInstance().playerCanMakeMove(move, gameplayController.getPlayer(), null, false).getKey()){
+                    canBuild.setColor(Color.LIGHTGREEN);
+                    imageView.setEffect(canBuild);
+                }
+                else{
+                    canBuild.setColor(Color.RED);
+                    imageView.setEffect(canBuild);
+                }
+
+                imageView.setOnMouseEntered( (e) -> {
+                    imageView.setScaleX(1);
+                    imageView.setScaleY(1);
+                });
+
+                imageView.setOnMouseExited( (e) -> {
+                    imageView.setScaleX(0.95);
+                    imageView.setScaleY(0.95);
+                });
+
                 imageView.setOnMouseClicked((e) ->  {
-                    if (focusedView != null) {
-                        focusedView.setScaleX(0.95);
-                        focusedView.setScaleY(0.95);
-                    }
                     if (focusedView != imageView) {
                         selectedCard = c;
+                        if(focusedView != null) {
+                            focusedView.setEffect(canBuild);
+                            focusedView.setScaleX(0.95);
+                            focusedView.setScaleY(0.95);
+                        }
                         focusedView = imageView;
                         imageView.setScaleX(1);
                         imageView.setScaleY(1);
+                        focusedView.setEffect(borderGlow);
+                    }
+                    else if (focusedView != null) {
+                        if(focusedView.getScaleX() == 1 && focusedView.getScaleY() == 1) {
+                            focusedView.setScaleX(0.95);
+                            focusedView.setScaleY(0.95);
+                            focusedView.setEffect(canBuild);
+                            selectedCard = null;
+                        }
+                        else{
+                            focusedView.setScaleX(1);
+                            focusedView.setScaleY(1);
+                            focusedView.setEffect(borderGlow);
+                        }
                     }
                 });
                 cardBox.getChildren().add(imageView);
@@ -82,5 +139,12 @@ public class CardViewController implements Initializable {
 
     public Card getSelectedCard(){
         return selectedCard;
+    }
+
+    @FXML
+    public void exitButtonClicked(MouseEvent e) {
+        Server.stopServerInstance();
+        gameplayController.getClient().disconnect();
+        SceneManager.getInstance().changeScene("MainMenu.fxml");
     }
 }
