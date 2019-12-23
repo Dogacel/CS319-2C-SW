14f2556eea1d20653dfac1782e1712d06b03e237
busra.buyukgebiz <busrabgz@gmail.com>
2019-12-23 22:56:17 +0300

    Load hero images in asset manager

--- src/src/main/java/SevenWonders/AssetManager.java
+++ src/src/main/java/SevenWonders/AssetManager.java
@@ -109,6 +109,15 @@ public class AssetManager {
             if( f.getName().matches(".*(\\.(png|jpg|jpeg))") && !imageMap.containsKey(f.getName()))
                 imageMap.put(f.getName(), new Image("/images/wonders/" + f.getName()));
         }
+
+        imageResourcesURL = getClass().getClassLoader().getResource("images/hero-images");
+        assert imageResourcesURL != null;
+        dir = new File(imageResourcesURL.getPath());
+
+        for (File f : Objects.requireNonNull(dir.listFiles())) {
+            if( f.getName().matches(".*(\\.(png|jpg|jpeg))") && !imageMap.containsKey(f.getName()))
+                imageMap.put(f.getName(), new Image("/images/hero-images/" + f.getName()));
+        }
     }
 
     private void loadScenes() {
