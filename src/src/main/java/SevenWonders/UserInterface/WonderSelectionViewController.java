db7643b1a0095cdd80fd0913a5a82de7b5543474
Yuce Hasan <yuceyuce99@gmail.com>
2019-12-25 22:27:04 +0300

    Add god images to wonder selection view

--- src/src/main/java/SevenWonders/UserInterface/WonderSelectionViewController.java
+++ src/src/main/java/SevenWonders/UserInterface/WonderSelectionViewController.java
@@ -9,9 +9,11 @@ import SevenWonders.SceneManager;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.sql.SQLOutput;
@@ -25,13 +27,20 @@ public class WonderSelectionViewController implements Initializable, ILobbyListe
     @FXML
     ImageView artemisImage, rhodesImage, gizaImage, babylonImage, halicarnassusImage, alexandriaImage, zeusImage;
 
-    ImageView artemis1, artemis2, artemis3;
-    ImageView rhodes1, rhodes2, rhodes3;
-    ImageView giza1, giza2, giza3;
-    ImageView babylon1, babylon2, babylon3;
-    ImageView halicarnassus1, halicarnassus2, halicarnassus3;
-    ImageView lightHouse1, lightHouse2, lightHouse3;
-    ImageView zeus1, zeus2, zeus3;
+    @FXML
+    ImageView artemis1, artemis2, artemis3, artemisGod;
+    @FXML
+    ImageView rhodes1, rhodes2, rhodes3, rhodesGod;
+    @FXML
+    ImageView giza1, giza2, giza3, gizaGod;
+    @FXML
+    ImageView babylon1, babylon2, babylon3, babylonGod;
+    @FXML
+    ImageView halicarnassus1, halicarnassus2, halicarnassus3, halicarnassusGod;
+    @FXML
+    ImageView lightHouse1, lightHouse2, lightHouse3, lighthouseGod;
+    @FXML
+    ImageView zeus1, zeus2, zeus3, zeusGod;
     @FXML
     Button button;
 
@@ -57,32 +66,58 @@ public class WonderSelectionViewController implements Initializable, ILobbyListe
         artemis1 = new ImageView(AssetManager.getInstance().getImage("templeofartemis_1.png"));
         artemis2 = new ImageView(AssetManager.getInstance().getImage("templeofartemis_2.png"));
         artemis3 = new ImageView(AssetManager.getInstance().getImage("templeofartemis_3.png"));
+        Tooltip artemisTp = new Tooltip("Artemis: Grants one victory point each turn after activation.");
+        artemisTp.setShowDelay(new Duration(250));
+        artemisTp.setHideDelay(new Duration(0));
+        Tooltip.install(artemisGod, artemisTp);
 
         rhodes1 = new ImageView(AssetManager.getInstance().getImage("colossusofrhodes_1.png"));
         rhodes2 = new ImageView(AssetManager.getInstance().getImage("colossusofrhodes_2.png"));
         rhodes3 = new ImageView(AssetManager.getInstance().getImage("colossusofrhodes_3.png"));
+        Tooltip rhodesTp = new Tooltip("Ares: Grants 5 shields to the user");
+        rhodesTp.setShowDelay(new Duration(250));
+        rhodesTp.setHideDelay(new Duration(0));
+        Tooltip.install(rhodesGod, rhodesTp);
 
         giza1 = new ImageView(AssetManager.getInstance().getImage("greatpyramidofgiza_1.png"));
         giza2 = new ImageView(AssetManager.getInstance().getImage("greatpyramidofgiza_2.png"));
         giza3 = new ImageView(AssetManager.getInstance().getImage("greatpyramidofgiza_3.png"));
+        Tooltip gizaTp = new Tooltip("Seth: Block the trades of all players.");
+        gizaTp.setShowDelay(new Duration(250));
+        gizaTp.setHideDelay(new Duration(0));
+        Tooltip.install(gizaGod, gizaTp);
 
         babylon1 = new ImageView(AssetManager.getInstance().getImage("hanginggardens_1.png"));
         babylon2 = new ImageView(AssetManager.getInstance().getImage("hanginggardens_2.png"));
         babylon3 = new ImageView(AssetManager.getInstance().getImage("hanginggardens_3.png"));
+        Tooltip babylonTp = new Tooltip("Marduk: Downgrade one stage of every player's wonder.");
+        babylonTp.setShowDelay(new Duration(250));
+        babylonTp.setHideDelay(new Duration(0));
+        Tooltip.install(babylonGod, babylonTp);
 
         halicarnassus1 = new ImageView(AssetManager.getInstance().getImage("mausoleumofhalicarnassus_1.png"));
         halicarnassus2 = new ImageView(AssetManager.getInstance().getImage("mausoleumofhalicarnassus_2.png"));
         halicarnassus3 = new ImageView(AssetManager.getInstance().getImage("mausoleumofhalicarnassus_3.png"));
+        Tooltip halicarnassusTp = new Tooltip("Athena: Can see the upcoming hand, every turn after you unlock the wonder.");
+        halicarnassusTp.setShowDelay(new Duration(250));
+        halicarnassusTp.setHideDelay(new Duration(0));
+        Tooltip.install(halicarnassusGod, halicarnassusTp);
 
         lightHouse1 = new ImageView(AssetManager.getInstance().getImage("lighthouseofalexandria_1.png"));
         lightHouse2 = new ImageView(AssetManager.getInstance().getImage("lighthouseofalexandria_2.png"));
         lightHouse3 = new ImageView(AssetManager.getInstance().getImage("lighthouseofalexandria_3.png"));
+        Tooltip lighthouseTp = new Tooltip("Ra: Delete one random science point from all players.");
+        lighthouseTp.setShowDelay(new Duration(250));
+        lighthouseTp.setHideDelay(new Duration(0));
+        Tooltip.install(lighthouseGod, lighthouseTp);
 
         zeus1 = new ImageView(AssetManager.getInstance().getImage("statueofzeus_1.png"));
         zeus2 = new ImageView(AssetManager.getInstance().getImage("statueofzeus_2.png"));
         zeus3 = new ImageView(AssetManager.getInstance().getImage("statueofzeus_3.png"));
-
-
+        Tooltip zeusTp = new Tooltip("Zeus: Block build and wonder upgrade actions of your neighbors.");
+        zeusTp.setShowDelay(new Duration(250));
+        zeusTp.setHideDelay(new Duration(0));
+        Tooltip.install(zeusGod, zeusTp);
     }
 
     public void artemisPressed(){
